54cb87a32cd6f030ab9ede129f0017415663caac
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-03-21 14:12:35 +0100

    Added initiation of bluetooth adapter

--- app/src/main/java/de/hs_kl/blesensor/OverviewActivity.java
+++ app/src/main/java/de/hs_kl/blesensor/OverviewActivity.java
@@ -1,13 +1,63 @@
 package de.hs_kl.blesensor;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class OverviewActivity extends Activity {
+public class OverviewActivity extends Activity
+{
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    private BluetoothAdapter btAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        if (null != savedInstanceState) return;
+
+        setupBluetoothAdapter();
+        ensureBluetoothIsEnabled();
+    }
+
+    private void setupBluetoothAdapter()
+    {
+        BluetoothManager btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        this.btAdapter = btManager.getAdapter();
+    }
+
+    private void ensureBluetoothIsEnabled()
+    {
+        if (this.btAdapter.isEnabled()) return;
+
+        Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetoothIntent, OverviewActivity.REQUEST_ENABLE_BT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if (OverviewActivity.REQUEST_ENABLE_BT == requestCode)
+        {
+            handleBluetoothEnableRequest(resultCode);
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, intent);
+        }
+    }
+
+    private void handleBluetoothEnableRequest(int resultCode)
+    {
+        if (RESULT_OK == resultCode) return;
+
+        Toast.makeText(this, R.string.bt_was_not_enabled, Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
