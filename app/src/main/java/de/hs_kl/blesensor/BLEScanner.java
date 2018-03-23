0d50916454fd9dd6e60215589fb92e872f2feefa
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-03-23 13:00:39 +0100

    Added a ble scanner which scans for devices and calls a provided callback with the results

---
+++ app/src/main/java/de/hs_kl/blesensor/BLEScanner.java
@@ -0,0 +1,102 @@
+package de.hs_kl.blesensor;
+
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanSettings;
+import android.os.Build;
+import android.os.Handler;
+import android.os.ParcelUuid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BLEScanner
+{
+    // TODO: Set UUID
+    private final static String BLE_SENSOR_SERVICE_UUID = "0x00000000-0000-0000-0000-000000000000";
+
+    private BluetoothLeScanner bleScanner;
+
+    public BLEScanner(BluetoothLeScanner bleScanner)
+    {
+        this.bleScanner = bleScanner;
+    }
+
+    public void scanForSensors(ScanCallback callback)
+    {
+        scanForSensorsForXMillis(callback, 10000);
+    }
+
+    public void scanForSensorsForXMillis(final ScanCallback callback, int millis)
+    {
+        this.bleScanner.startScan(buildScanFilters(), buildScanSettings(), callback);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                BLEScanner.this.bleScanner.stopScan(callback);
+            }
+        }, millis);
+    }
+
+    public void scanForSensorData(ScanCallback callback, List<String> deviceAddresses)
+    {
+        List<ScanFilter> filters = buildScanFiltersForSpecificDevices(deviceAddresses);
+        ScanSettings settings = buildScanSettings();
+        this.bleScanner.startScan(filters, settings, callback);
+    }
+
+    private List<ScanFilter> buildScanFilters()
+    {
+        List<ScanFilter> scanFilters = new ArrayList<>();
+        ScanFilter.Builder builder = new ScanFilter.Builder();
+        builder.setServiceUuid(ParcelUuid.fromString(BLEScanner.BLE_SENSOR_SERVICE_UUID));
+        scanFilters.add(builder.build());
+        return scanFilters;
+    }
+
+    private List<ScanFilter> buildScanFiltersForSpecificDevices(List<String> deviceAddresses)
+    {
+        List<ScanFilter> scanFilters = new ArrayList<>();
+        for (String deviceAddress: deviceAddresses)
+        {
+            ScanFilter.Builder builder = new ScanFilter.Builder();
+            builder.setServiceUuid(ParcelUuid.fromString(BLEScanner.BLE_SENSOR_SERVICE_UUID));
+            builder.setDeviceAddress(deviceAddress);
+            scanFilters.add(builder.build());
+        }
+        return scanFilters;
+    }
+
+    private ScanSettings buildScanSettings()
+    {
+        ScanSettings.Builder builder = new ScanSettings.Builder();
+        builder.setReportDelay(0);
+        builder.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            builder.setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES);
+            builder.setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE);
+            builder.setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT);
+        }
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            builder.setLegacy(false);
+            builder.setPhy(ScanSettings.PHY_LE_ALL_SUPPORTED);
+        }
+        return builder.build();
+    }
+
+    public void stopScanning()
+    {
+        stopScanning(null);
+    }
+
+    public void stopScanning(ScanCallback callback)
+    {
+        this.bleScanner.stopScan(callback);
+    }
+}
