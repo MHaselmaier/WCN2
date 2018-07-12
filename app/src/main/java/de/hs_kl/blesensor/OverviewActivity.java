b73e30b1cd8527a200bddeb805fef5eb703f30f1
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-07-12 16:16:32 +0200

    The user is asked to turn on the location service, if its not on, the app will be shut down.

--- app/src/main/java/de/hs_kl/blesensor/OverviewActivity.java
+++ app/src/main/java/de/hs_kl/blesensor/OverviewActivity.java
@@ -1,21 +1,27 @@
 package de.hs_kl.blesensor;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import de.hs_kl.blesensor.ble_scanner.BLEScanner;
 import de.hs_kl.blesensor.fragments.actions.ActionsFragment;
@@ -41,6 +47,14 @@ public class OverviewActivity extends AppCompatActivity
         }
     };
 
+    private BroadcastReceiver locationModeChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            ensureLocationIsEnabled();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -54,6 +68,9 @@ public class OverviewActivity extends AppCompatActivity
         registerReceiver(this.btAdapterChangeReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
         setupBluetoothAdapter();
         ensureBluetoothIsEnabled();
+
+        registerReceiver(this.locationModeChangeReceiver, new IntentFilter((LocationManager.MODE_CHANGED_ACTION)));
+        ensureLocationIsEnabled();
 
         final DrawerLayout drawer = findViewById(R.id.drawer);
         View overview = findViewById(R.id.overview);
@@ -160,12 +177,68 @@ public class OverviewActivity extends AppCompatActivity
         }
     }
 
+    private void ensureLocationIsEnabled()
+    {
+        try
+        {
+            if (0 != Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE))
+            {
+                BLEScanner.setBluetoothLeScanner(this.btAdapter.getBluetoothLeScanner());
+                return;
+            }
+        }
+        catch (Exception e) {}
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage(R.string.enable_location);
+
+        final AtomicBoolean accepted = new AtomicBoolean(false);
+        dialogBuilder.setPositiveButton(R.string.allow, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Intent enableLocationIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(enableLocationIntent, Constants.REQUEST_ENABLE_LOCATION);
+                accepted.set(true);
+            }
+        });
+
+        dialogBuilder.setNegativeButton(R.string.deny, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        dialogBuilder.setOnDismissListener(new DialogInterface.OnDismissListener()
+        {
+            @Override
+            public void onDismiss(DialogInterface dialog)
+            {
+                if (!accepted.get())
+                {
+                    Toast.makeText(OverviewActivity.this, R.string.denied_permission_location, Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        });
+
+        dialogBuilder.show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent)
     {
         if (Constants.REQUEST_ENABLE_BT == requestCode)
         {
             handleBluetoothEnableRequest(resultCode);
+        }
+        else if (Constants.REQUEST_ENABLE_LOCATION == requestCode)
+        {
+            handleLocationEnableRequest();
         }
         else
         {
@@ -181,9 +254,23 @@ public class OverviewActivity extends AppCompatActivity
         }
         else
         {
-            Toast.makeText(this, R.string.bt_was_not_enabled, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.bt_was_not_enabled, Toast.LENGTH_LONG).show();
             finish();
         }
+    }
+
+    private void handleLocationEnableRequest()
+    {
+        try {
+            if (0 == Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE)) {
+                Toast.makeText(OverviewActivity.this, R.string.denied_permission_location, Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+            BLEScanner.setBluetoothLeScanner(this.btAdapter.getBluetoothLeScanner());
+        }
+        catch(Exception e) {}
+
     }
 
     @Override
--- app/src/main/java/de/hs_kl/blesensor/util/Constants.java
+++ app/src/main/java/de/hs_kl/blesensor/util/Constants.java
@@ -3,7 +3,8 @@ package de.hs_kl.blesensor.util;
 public class Constants
 {
     public static final int REQUEST_ENABLE_BT = 1;
-    public static final int REQUEST_PERMISSIONS = 2;
+    public static final int REQUEST_ENABLE_LOCATION = 2;
+    public static final int REQUEST_PERMISSIONS = 3;
 
     public static final int MANUFACTURER_ID = 0xFFFF;
 
