b43fa803c47d234031e847d549ae3de1feae6e1b
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-07-26 11:01:41 +0200

    Defined actions now keep their orderning even when the app starts again.

--- app/src/main/java/de/hs_kl/wcn2/util/DefinedActionStorage.java
+++ app/src/main/java/de/hs_kl/wcn2/util/DefinedActionStorage.java
@@ -3,32 +3,77 @@ package de.hs_kl.wcn2.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 public class DefinedActionStorage
 {
+    private static List<String> actions;
+
     public static String[] getDefinedActions(Context context)
     {
-        Set<String> actions = context.getSharedPreferences(Constants.DEFINED_ACTIONS, Context.MODE_PRIVATE)
-                                     .getAll().keySet();
-        return actions.toArray(new String[0]);
+        if (null == DefinedActionStorage.actions)
+        {
+            Object[] entries = context.getSharedPreferences(Constants.DEFINED_ACTIONS,
+                    Context.MODE_PRIVATE).getAll().entrySet().toArray();
+            Arrays.sort(entries, new Comparator<Object>()
+            {
+                @Override
+                public int compare(Object o1, Object o2)
+                {
+                    return ((Map.Entry<String, Integer>)o1).getValue() - ((Map.Entry<String, Integer>)o2).getValue();
+                }
+            });
+
+            DefinedActionStorage.actions = new ArrayList<>();
+            for (Object entry: entries)
+            {
+                DefinedActionStorage.actions.add(((Map.Entry<String, Integer>)entry).getKey());
+            }
+        }
+        return DefinedActionStorage.actions.toArray(new String[DefinedActionStorage.actions.size()]);
     }
 
     public static void addAction(Context context, String action)
     {
+        getDefinedActions(context);
+        if (DefinedActionStorage.actions.contains(action))
+        {
+            return;
+        }
+        DefinedActionStorage.actions.add(action);
+
         SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.DEFINED_ACTIONS,
                 Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(action, action);
-        editor.commit();
+        editor.putInt(action, DefinedActionStorage.actions.size() - 1);
+        editor.apply();
     }
 
     public static void removeAction(Context context, String action)
     {
+        getDefinedActions(context);
+        int index = DefinedActionStorage.actions.indexOf(action);
+        DefinedActionStorage.actions.remove(action);
+
         SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.DEFINED_ACTIONS,
                 Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove(action);
-        editor.commit();
+        moveActionsUpStartingFrom(index, editor);
+        editor.apply();
+    }
+
+    private static void moveActionsUpStartingFrom(int startIndex, SharedPreferences.Editor editor)
+    {
+        if (0 > startIndex || DefinedActionStorage.actions.size() <= startIndex) return;
+
+        for (int i = startIndex; DefinedActionStorage.actions.size() > i; ++i)
+        {
+            editor.putInt(DefinedActionStorage.actions.get(i), i);
+        }
     }
 }
