87f300aceab47a9cb81774954ff5eb549b292b04
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-09-13 10:45:34 +0200

    The sensors are now tracked/untracked in a background thread. This should result in a mroe smooth ui.

--- app/src/main/java/de/hs_kl/wcn2/fragments/search_sensor/SensorTrackedChangeListener.java
+++ app/src/main/java/de/hs_kl/wcn2/fragments/search_sensor/SensorTrackedChangeListener.java
@@ -3,19 +3,41 @@ package de.hs_kl.wcn2.fragments.search_sensor;
 import android.content.Context;
 import android.widget.CompoundButton;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 import de.hs_kl.wcn2.R;
 import de.hs_kl.wcn2.ble_scanner.SensorData;
 import de.hs_kl.wcn2.util.TrackedSensorsStorage;
 
 public class SensorTrackedChangeListener implements CompoundButton.OnCheckedChangeListener
 {
-    private Context context;
+    private static ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
+    static
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    while (true)
+                    {
+                        SensorTrackedChangeListener.queue.take().run();
+                    }
+                }
+                catch (InterruptedException e) {}
+            }
+        }).start();
+    }
+
     private SensorData sensorData;
+    private TrackedSensorsStorage trackedSensors;
 
     public SensorTrackedChangeListener(Context context, SensorData sensorData)
     {
-        this.context = context;
         this.sensorData = sensorData;
+        this.trackedSensors = TrackedSensorsStorage.getInstance(context);
     }
 
     @Override
@@ -23,12 +45,28 @@ public class SensorTrackedChangeListener implements CompoundButton.OnCheckedChan
     {
         if (isChecked)
         {
-            TrackedSensorsStorage.getInstance(this.context).trackSensor(this.sensorData);
+            SensorTrackedChangeListener.queue.add(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    SensorTrackedChangeListener.this.trackedSensors.trackSensor(
+                            SensorTrackedChangeListener.this.sensorData);
+                }
+            });
             buttonView.setText(R.string.sensor_tracked);
         }
         else
         {
-            TrackedSensorsStorage.getInstance(this.context).untrackSensor(this.sensorData);
+            SensorTrackedChangeListener.queue.add(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    SensorTrackedChangeListener.this.trackedSensors.untrackSensor(
+                            SensorTrackedChangeListener.this.sensorData);
+                }
+            });
             buttonView.setText(R.string.sensor_not_tracked);
         }
     }
