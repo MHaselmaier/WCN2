0a94867f0d22e769b324460d75fff66aaf2d4010
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2019-06-17 10:21:24 +0200

    Added notification channel group for alarms.

--- wcn2_alarm/src/main/java/de/hs_kl/wcn2_alarm/alarm_triggered_service/AlarmNotifications.java
+++ wcn2_alarm/src/main/java/de/hs_kl/wcn2_alarm/alarm_triggered_service/AlarmNotifications.java
@@ -3,6 +3,7 @@ package de.hs_kl.wcn2_alarm.alarm_triggered_service;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -16,52 +17,69 @@ import de.hs_kl.wcn2_alarm.alarms.WCN2Alarm;
 public class AlarmNotifications
 {
     private final static String SERVICE_CHANNEL_ID = "AlarmTriggerService";
+    private final static String ALARM_CHANNEL_GROUP_ID = "Alarms";
 
     private final static int LIGHTS_COLOR = 0xFFFF0000;
     private final static long[] VIBRATION_PATTERN = {0, 500};
 
     static void createServiceNotificationChannel(Context context)
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            createNotificationChannel(context, NotificationManager.IMPORTANCE_LOW,
-                    AlarmNotifications.SERVICE_CHANNEL_ID, "AlarmTriggerService",
-                    "This channel is used to indicate to the user, that his alarms are monitored.", false, false);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
+
+        createNotificationChannel(context, NotificationManager.IMPORTANCE_LOW,
+            AlarmNotifications.SERVICE_CHANNEL_ID, "AlarmTriggerService",
+                "This channel is used to indicate to the user, that his alarms are monitored.", null,
+                false, false);
     }
 
     public static void createAlarmNotificationChannel(Context context, String alarm)
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            createNotificationChannel(context, NotificationManager.IMPORTANCE_HIGH,
-                    alarm, alarm,
-                    "This channel is used to notify the user when the " + alarm + " alarm is triggered", true, true);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
+
+        createAlarmNotificationGroup(context);
+        createNotificationChannel(context, NotificationManager.IMPORTANCE_HIGH,
+                alarm, alarm,
+                    "This channel is used to notify the user when the " + alarm + " alarm is triggered",
+                AlarmNotifications.ALARM_CHANNEL_GROUP_ID, true, true);
+    }
+
+    private static void createAlarmNotificationGroup(Context context)
+    {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
+
+        NotificationManager manager = (NotificationManager)context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (null == manager) return;
+
+        manager.createNotificationChannelGroup(new NotificationChannelGroup(
+                AlarmNotifications.ALARM_CHANNEL_GROUP_ID, "Alarms"));
     }
 
     private static void createNotificationChannel(Context context, int importance, String id,
-                                                  String name, String description,
+                                                  String name, String description, String groupID,
                                                   boolean showLights, boolean vibrate)
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            NotificationManager manager = (NotificationManager)context
-                    .getSystemService(Activity.NOTIFICATION_SERVICE);
-            if (null != manager && null == manager.getNotificationChannel(id))
-            {
-                NotificationChannel channel = new NotificationChannel(id, name, importance);
-                channel.setDescription(description);
-                if (showLights)
-                {
-                    channel.setLightColor(AlarmNotifications.LIGHTS_COLOR);
-                    channel.enableLights(true);
-                }
-                if (vibrate)
-                {
-                    channel.setVibrationPattern(AlarmNotifications.VIBRATION_PATTERN);
-                    channel.enableVibration(true);
-                }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
 
-                manager.createNotificationChannel(channel);
-            }
+        NotificationManager manager = (NotificationManager)context
+                .getSystemService(Activity.NOTIFICATION_SERVICE);
+        if (null == manager || null != manager.getNotificationChannel(id)) return;
+
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+        channel.setDescription(description);
+        channel.setGroup(groupID);
+        if (showLights)
+        {
+            channel.setLightColor(AlarmNotifications.LIGHTS_COLOR);
+            channel.enableLights(true);
         }
+        if (vibrate)
+        {
+            channel.setVibrationPattern(AlarmNotifications.VIBRATION_PATTERN);
+            channel.enableVibration(true);
+        }
+
+        manager.createNotificationChannel(channel);
     }
 
     static Notification createServiceNotification(Context context)
